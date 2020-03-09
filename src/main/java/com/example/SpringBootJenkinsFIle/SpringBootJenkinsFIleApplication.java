f28845cf16551e5fa4ac4d44d7b5c79f527c8cf4
Michal <michal.malczyk@gmail.com>
2020-03-09 14:08:06 +0100

    first commit

---
+++ src/main/java/com/example/SpringBootJenkinsFIle/SpringBootJenkinsFIleApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootJenkinsFIle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinsFIleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenkinsFIleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootJenkinsFIle/SpringBootJenkinsFIleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootJenkinsFIle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsFIleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
